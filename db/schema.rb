864b058f0780ed3e870c87b1549d2462e41fe83e
Louie Yap <LouieY@magenic.com>
2018-08-31 15:07:09 +0800

    database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2018_08_30_051739) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "adminpack"
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
