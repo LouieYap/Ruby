1b8321da9d5cbe1c555734cfeaf48df79e7d7e0f
Louie Yap <LouieY@magenic.com>
2018-09-06 13:32:41 +0800

    test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,18 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
-  def setup
-    @user = User.new(name: "Example User", email: "user@example.com", password: "", password_confirmation: "")
-  end
-
-  test "password should be present (nonblank)" do
-    @user.password = @user.password_confirmation = " " * 6
-    assert_not @user.valid?
-  end
-
-  test "password should have a minimum length" do
-    @user.password = @user.password_confirmation = "a" * 5
-    assert_not @user.valid?
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
