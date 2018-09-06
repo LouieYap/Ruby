c6d96d25c8070672e59f175d8d81f6fd0b6ae4f3
Louie Yap <LouieY@magenic.com>
2018-09-06 16:15:14 +0800

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   get '/login', to: 'login#login'
   get '/signup', to: 'users#new'
   get '/home', to: 'static_pages#home'
